78663cced5f779343efd3385b7a78d8cb2235080
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-08-08 21:16:18 -0300

    food service added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  foods:String[] = [];
+  constructor(private foodService:FoodService) { }
 
   ngOnInit(): void {
+    this.foods = this.foodService.getAll();
   }
 
 }
